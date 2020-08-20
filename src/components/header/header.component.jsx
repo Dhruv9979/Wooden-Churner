55788aa1d01ed9c796c4a61a45a74f3216a7db97
Dhruv Patel <starb9979@gmail.com>
2020-08-20 02:56:48 -0400

    added checkout page, checkout item component and update cart to use select page from selectors

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
-import CartDropdown from '../cart/cart-dropdown.component';
+import CartDropdown from "../cart/cart-dropdown.component";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
 
 import { ReactComponent as Logo } from "../../images/logo.svg";
 
@@ -36,13 +39,13 @@ const Header = ({ currentUser, hidden }) => (
 			)}
 			<CartIcon />
 		</div>
-		{ hidden ? null: <CartDropdown /> }
+		{hidden ? null : <CartDropdown />}
 	</div>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-	currentUser,
-	hidden,
+const mapStateToProps = createStructuredSelector({
+	currentUser: selectCurrentUser,
+	hidden: selectCartHidden,
 });
 
 export default connect(mapStateToProps)(Header);

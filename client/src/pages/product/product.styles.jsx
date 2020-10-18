91602d65f57359fc66b035251cf14ee8e185f41f
Dhruv Patel <starb9979@gmail.com>
2020-10-18 16:00:43 -0400

    adding performance optimizations to codebase with react lazy, suspense and memo

--- client/src/pages/product/product.styles.jsx
+++ client/src/pages/product/product.styles.jsx
@@ -14,7 +14,13 @@ export const ProductItemsContainer = styled.div`
 	display: grid;
 	grid-template-columns: 1fr 1fr 1fr 1fr;
 	grid-gap: 10px;
+	
 	& > div {
 		margin-bottom: 30px;
+	}
+
+	@media screen and (max-width: 800px) {
+		grid-template-columns: 1fr 1fr;
+		grid-gap: 15px;
 	}
 `;

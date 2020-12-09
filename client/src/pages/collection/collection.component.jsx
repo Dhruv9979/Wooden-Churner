61a32c88520d478c38b6b3faca05fa99cdf509cb
Dhruv9979 <starb9979@gmail.com>
2020-12-08 20:16:18 -0500

    added left arrow font awesome icon to collections and products pages.

--- client/src/pages/collection/collection.component.jsx
+++ client/src/pages/collection/collection.component.jsx
@@ -1,35 +1,43 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
-import CollectionItem from "../../components/collection-item/collection-item.component";
+import CollectionItem from '../../components/collection-item/collection-item.component';
 
-import { selectCollection } from "../../redux/shop/shop.selectors";
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import {
-	CollectionPageContainer,
-	CollectionTitle,
-	CollectionItemsContainer,
-	OptionButton
-} from "./collection.styles";
+    CollectionPageContainer,
+    CollectionTitle,
+    CollectionItemsContainer,
+    OptionButton,
+} from './collection.styles';
 
 export const CollectionPage = ({ collection, history }) => {
-	const { title, items } = collection;
-	const displayCollectionItems = Object.entries(items).map(([key, val]) => (
-		<CollectionItem key={val.id} item={val} />
-	));
-	return (
-		<CollectionPageContainer>
-		<OptionButton onClick={() => history.goBack()}>GO BACK</OptionButton>
-			<CollectionTitle>{title}</CollectionTitle>
-			<CollectionItemsContainer>
-				{displayCollectionItems}
-			</CollectionItemsContainer>
-		</CollectionPageContainer>
-	);
+    const { title, items } = collection;
+    const displayCollectionItems = Object.entries(items).map(([key, val]) => (
+        <CollectionItem key={val.id} item={val} />
+    ));
+    return (
+        <CollectionPageContainer>
+            <OptionButton onClick={() => history.goBack()}>
+                <FontAwesomeIcon
+                    icon={faArrowLeft}
+                    style={{ marginRight: '10px' }}
+                />
+                GO BACK
+            </OptionButton>
+            <CollectionTitle>{title}</CollectionTitle>
+            <CollectionItemsContainer>
+                {displayCollectionItems}
+            </CollectionItemsContainer>
+        </CollectionPageContainer>
+    );
 };
 
 const mapStateToProps = (state, ownProps) => ({
-	collection: selectCollection(ownProps.match.params.collectionId)(state),
+    collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
 
 export default connect(mapStateToProps)(CollectionPage);
--- client/src/pages/product/product.component.jsx
+++ client/src/pages/product/product.component.jsx
@@ -1,44 +1,50 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
-import CollectionItem from "../../components/collection-item/collection-item.component";
+import CollectionItem from '../../components/collection-item/collection-item.component';
 // import ProductInformation  from '../../components/product-information/product-information.component';
 
-import { selectProducts } from "../../redux/shop/shop.selectors";
+import { selectProducts } from '../../redux/shop/shop.selectors';
 
 import {
-	ProductPageContainer,
-	ProductTitle,
-	ProductItemsContainer,
-	OptionButton
-} from "./product.styles";
+    ProductPageContainer,
+    ProductTitle,
+    ProductItemsContainer,
+    OptionButton,
+} from './product.styles';
 
 export const ProductPage = ({ items, history }) => {
-	console.log(history);
-
-	const { name, products } = items;
-	return (
-		<ProductPageContainer>
-			<OptionButton onClick={() => history.goBack()}>GO BACK</OptionButton>
-			<ProductTitle>{name}</ProductTitle>
-			{/* {Object.entries(description).map(([key, val]) => 
+    const { name, products } = items;
+    return (
+        <ProductPageContainer>
+            <OptionButton onClick={() => history.goBack()}>
+                <FontAwesomeIcon
+                    icon={faArrowLeft}
+                    style={{ marginRight: '10px' }}
+                />
+                GO BACK
+            </OptionButton>
+            <ProductTitle>{name}</ProductTitle>
+            {/* {Object.entries(description).map(([key, val]) => 
 				<ProductInformation h2 = {h2} p = {val.p} li = {val.li} />
 			)} */}
-			{/* <ProductInformation description={description} /> */}
-			<ProductItemsContainer>
-				{Object.entries(products).map(([key, val]) => (
-					<CollectionItem key={val.id} item={val} />
-				))}
-			</ProductItemsContainer>
-		</ProductPageContainer>
-	);
+            {/* <ProductInformation description={description} /> */}
+            <ProductItemsContainer>
+                {Object.entries(products).map(([key, val]) => (
+                    <CollectionItem key={val.id} item={val} />
+                ))}
+            </ProductItemsContainer>
+        </ProductPageContainer>
+    );
 };
 
 const mapStateToProps = (state, ownProps) => ({
-	items: selectProducts(
-		ownProps.match.params.collectionId,
-		ownProps.match.params.productId
-	)(state),
+    items: selectProducts(
+        ownProps.match.params.collectionId,
+        ownProps.match.params.productId
+    )(state),
 });
 
 export default connect(mapStateToProps)(ProductPage);
